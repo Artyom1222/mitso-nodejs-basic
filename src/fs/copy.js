8ed74d1e06c24974d4081e1c7f9be924c0258595
Artyom1222 <kovalenkaa2@mail.ru>
2025-04-14 08:44:05 +0300

    Copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,34 @@
+import fs from 'fs/promises';
+import { join } from 'path';
+import { getPath } from "../utils/getPath.js";
+
 const copy = async () => {
-    // Write your code here 
+    const { __dirname } = getPath(import.meta.url);
+    const sourceFolder = join(__dirname, 'files');
+    const destinationFolder = join(__dirname, 'files_copy');
+
+    try {
+        await fs.access(sourceFolder);
+    } catch (err) {
+        console.error('FS operation failed');
+        return;
+    }
+
+    try {
+        await fs.access(destinationFolder);
+        console.error('FS operation failed');
+        return;
+    } catch (err) {}
+
+    await fs.mkdir(destinationFolder, { recursive: true });
+
+    const items = await fs.readdir(sourceFolder);
+    for (const item of items) {
+        const sourceItem = join(sourceFolder, item);
+        const destinationItem = join(destinationFolder, item);
+
+        await fs.copyFile(sourceItem, destinationItem);
+    }
 };
 
 await copy();
